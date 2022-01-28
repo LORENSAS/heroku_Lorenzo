9cf10d0ca6ca2f30123e4c843977cacf368ca1f2
isaac159 <isaacpmagalhaesp@gmail.com>
2022-01-28 20:07:10 -0300

    first commit

---
+++ sagaz.php
@@ -0,0 +1,86 @@
+<?php
+
+>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test de cadastro - Lorens</title>
+    <link rel="stylesheet" href="style_pagina_decadastro.css">
+    
+    
+</head>
+
+
+
+<body>
+    <main>
+            <h1>Cadastro</h1>
+
+        <div class="social">
+            
+                <a href="https://accounts.google.com/AddSession/signinchooser?service=accountsettings&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Futm_source%3Dsign_in_no_continue&ec=GAlAwAE&flowName=GlifWebSignIn&flowEntry=AddSession">
+                    <img src="img_de_sites_de_cadastro/google.png"  alt="google">
+                </a>
+            
+                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj3qff-vsTxAhVHlJUCHdzQAgMQFjAAegQIBBAD&url=https%3A%2F%2Fbr.linkedin.com%2F&usg=AOvVaw2SBY7l_GSJomHDs9LB4qMx">
+                    <img src="img_de_sites_de_cadastro/linkedin.png" alt="linkedin">
+                </a> 
+
+                <a href="https://www.facebook.com">
+                    <img src="img_de_sites_de_cadastro/facebook.png" alt="facebook">
+                </a>
+            <br>
+        </div>
+            
+            <form action="sas">
+                <label for="name"></label>
+
+                <span>Nome:</span>
+
+                <input type="text" id="name" name="name">
+            </form>
+
+            <form action="">
+                <label for="name"></label>
+
+                <span>E-mail:</span>
+
+                <input type="E-mail" id="E-mail" name="E-mail">
+            </form>
+
+            <form action="submit">
+                <label for="name"></label>
+
+                <span>Senha:</span>
+
+                <input type="Password" id="Password" name="Password">
+            </label>
+                
+                <input type="submit" class="submit">
+
+                <div class="privacidade">
+                    <h2>
+                        <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi7kPqZv8TxAhUNpZUCHUlKBEIQFjAAegQIBhAD&url=https%3A%2F%2Fpolicies.google.com%2Fprivacy%3Fhl%3Dpt-BR&usg=AOvVaw1kltNKP4asNcojLXtY516S"alt="poliica_de_privacidade">Politicas de privacidade</a>
+                    </h2>
+                </div>
+            </form>
+            <br>
+            <br>
+               
+            
+                
+                <section class="images">
+
+                    <img src="img_de_sites_de_cadastro/cadastro.png" width="350px" height="350px" alt="Mobile">
+            
+
+                
+                
+            </section>
+    </main>
+
+</body>
+</html>
